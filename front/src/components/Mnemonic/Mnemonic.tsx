861d25a2594bb444f0ed97ed081208009fdc684a
char1ey <ckstn410@gmail.com>
2023-06-19 10:46:01 +0900

    Feat: Mnemonic 페이지 완료

--- front/src/components/Mnemonic/Mnemonic.tsx
+++ front/src/components/Mnemonic/Mnemonic.tsx
@@ -1,71 +1,34 @@
-import styled from 'styled-components'
 import { Icon } from '@iconify/react';
-import { useState } from "react";
+import { MouseEvent, useState } from "react";
+import { CopyButton } from "@components/CopyButton/CopyButton";
+import { MnemonicBoxWrap, MnemonicBox, MnemonicContent, MnemonicVisible } from "./styled/Mnemonic.styled";
 
-export interface ISizeProps {
-    width?: string
-    height?: string
+interface IMnemonic {
+    mnemonic: string[]
 }
 
-interface IMnemonicBox extends ISizeProps{
-    bgColor?: string
-}
+// const mnemonic = ["index", "salmon", "victory", "noise", "lava", "zebra", "pudding", "regular", "acoustic", "rule", "forget", "must"]
+export const Mnemonic = ({ mnemonic }: IMnemonic) => {
+    const [isVisible, setVisible] = useState(false)
+    const [blur, setBlur] = useState('0')
 
-interface IMenmonicContent extends ISizeProps{
-    blur?: string
-}
+    const handleVisible = (e: MouseEvent<HTMLDivElement>) => {
+        setVisible(!isVisible)
+        if(blur === '0') setBlur('0.25rem')
+        if(blur === '0.25rem') setBlur('0')
+    }
 
-const MnemonicBoxWrap = styled.div<ISizeProps>`
-    display: flex;
-    justify-content: center;
-    width: ${(props) => props.width || "100%"};
-    height: ${(props) => props.height || "100%"};
-`
-
-const MnemonicBox = styled.p<IMnemonicBox>`
-    position: relative;
-    padding: 1rem 2rem ;
-    width: ${(props) => props.width || "100%"};
-    min-height: 9rem;
-    height: ${(props) => props.height || "100%"};
-    background-color: ${(props) => props.bgColor || "#555555"};
-    box-sizing: border-box;
-`
-
-const MnemonicContent = styled.p<IMenmonicContent>`
-    width: ${(props) => props.width || "100%"};
-    height: ${(props) => props.height || "100%"};
-    color: ${(props) => props.color || "#888888"};
-    font-size: 1.6rem;
-    font-weight: 400;
-    filter: blur(${(props) => props.blur || "0.4rem"});
-`
-
-const MnemonicVisible = styled.div<ISizeProps>`
-    position: absolute;
-    right: 1.5rem;
-    bottom: 0rem;
-    width: ${(props) => props.width || "100%"};
-    font-size: 2.5rem;
-    color: white;
-`
-
-const mnemonic = ["index", "salmon", "victory", "noise", "lava", "zebra", "pudding", "regular", "acoustic", "rule", "forget", "must"]
-
-export const Mnemonic = () => {
-    const [visible, isVisible] = useState(false)
-    
-    return(
+    return (
         <MnemonicBoxWrap height="15%">
             <MnemonicBox width="85%" height="80%">
-                <MnemonicContent blur={'0'}>
+                <MnemonicContent blur={blur}>
                     {mnemonic.join(' ')}
                 </MnemonicContent>
-                <MnemonicVisible width="2.5rem">
-                    <Icon icon="mdi:eye-off" hFlip={true} />
-                    <Icon icon="heroicons:eye-solid" hFlip={true} />
+                <MnemonicVisible width="2.5rem" onClick={handleVisible}>
+                    {isVisible ? <Icon icon="mdi:eye-off" hFlip={true} /> : <Icon icon="heroicons:eye-solid" hFlip={true} />}
                 </MnemonicVisible>
             </MnemonicBox>
+            <CopyButton />
         </MnemonicBoxWrap>
     )
 }
---
+++ front/src/components/Mnemonic/styled/Mnemonic.styled.tsx
@@ -0,0 +1,39 @@
+import styled from "styled-components"
+import { ISizeProps, IMnemonicBox, IMenmonicContent } from "./interface"
+
+export const MnemonicBoxWrap = styled.div<ISizeProps>`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: ${(props) => props.width || "100%"};
+    height: ${(props) => props.height || "100%"};
+`
+
+export const MnemonicBox = styled.div<IMnemonicBox>`
+    position: relative;
+    padding: 1rem 2rem ;
+    width: ${(props) => props.width || "100%"};
+    min-height: 9rem;
+    height: ${(props) => props.height || "100%"};
+    background-color: ${(props) => props.bgColor || "#555555"};
+    box-sizing: border-box;
+`
+
+export const MnemonicContent = styled.div<IMenmonicContent>`
+    width: ${(props) => props.width || "100%"};
+    height: ${(props) => props.height || "100%"};
+    color: ${(props) => props.color || "#888888"};
+    font-size: 1.6rem;
+    font-weight: 400;
+    filter: blur(${(props) => props.blur || "0.4rem"});
+`
+
+export const MnemonicVisible = styled.div<ISizeProps>`
+    cursor: pointer;
+    position: absolute;
+    right: 1.5rem;
+    bottom: 0rem;
+    width: ${(props) => props.width || "100%"};
+    font-size: 2.5rem;
+    color: white;
+`
