a20fd88f8d631adcb1eb51e51ea2ec018f19478b
BAEK'space <112856532+100space@users.noreply.github.com>
2023-06-20 19:31:47 +0900

    Fix : ESLint 추가

--- front/src/styled/index.tsx
+++ front/src/styled/index.tsx
@@ -12,12 +12,15 @@ export const FullScreen = css`
     height: 100%;
 `
 export const RootWrap = styled.div<ISizeProps>`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-direction: column;
     width: 100%;
     height: 100%;
     padding: 3rem;
     background: ${({ theme, mode }) => mode && theme[mode].basicBg};
 `
-
 export const SizePropsStyled = css<ISizeProps>`
     width: ${(props) => props.width || "100%"};
     height: ${(props) => props.height || "100%"};
