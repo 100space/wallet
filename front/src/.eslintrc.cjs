7deeeb017c20e204ab3b6f1b4b147af07e2db712
BAEK'space <112856532+100space@users.noreply.github.com>
2023-06-20 20:07:03 +0900

    Test : ESlint Test

--- front/src/.eslintrc.cjs
+++ front/src/.eslintrc.cjs
@@ -1,7 +1,7 @@
 module.exports = {
     rules: {
-        // Note: you must disable the base rule as it can report incorrect errors
         "no-unused-vars": "off",
-        "@typescript-eslint/no-unused-vars": "error",
+        "@typescript-eslint/no-unused-vars": "off",
+        "react-hooks/exhaustive-deps": "off",
     },
 }
