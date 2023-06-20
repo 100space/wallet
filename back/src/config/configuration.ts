bc3c4a2af51f6a4b6f8f2e4d315ae77ecf7719c4
cloudcoke <lms4296@gmail.com>
2023-06-20 20:37:15 +0900

    Fix : test_back.yaml 파일 수정

---
+++ back/src/config/configuration.ts
@@ -0,0 +1,7 @@
+export default () => ({
+  port: parseInt(process.env.PORT, 10) || 3000,
+  database: {
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT, 10) || 5432,
+  },
+});
