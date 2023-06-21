0f65479c01fa20cead78a7425fe081805cbccc8d
root <root@Char1ey>
2023-06-22 03:13:16 +0900

    Feat: common/CoinInfo 컴포넌트

---
+++ front/src/common/infomation/styled/CoinDescription.styled.tsx
@@ -0,0 +1,40 @@
+import { useGetMode } from "@hooks/useMode"
+import { SizePropsStyled } from "@styled/index"
+import { ISizeProps } from "@utils/interFace/styled.interface"
+import { styled } from "styled-components"
+
+export const CoinDescriptionWrap = styled.div<ISizeProps>`
+    ${SizePropsStyled};
+    padding: 2rem;
+`
+
+export const CoinDescriptionSubject = styled.header<ISizeProps>`
+    margin: 1rem 0;
+    font-size: 2.4rem;
+    font-weight: 500;
+    color: ${({mode, theme}) => mode && theme[mode].text};
+`
+
+export const CoinDescriptionContent = styled.div<ISizeProps>`
+    ${SizePropsStyled};
+    padding: 1rem 2rem;
+    background-color: ${({ mode, theme}) => mode && theme[mode].bgDescription};
+    font-size: 1.4rem;
+    color: ${({mode, theme}) => mode && theme[mode].text};
+    border-radius: 1rem;
+`
+
+export const CoinDescription = ( props : {description: string, name: string}) => {
+    const [modeState, setChange] = useGetMode()
+
+    return(
+        <CoinDescriptionWrap>
+            <CoinDescriptionSubject mode={modeState.mode}>
+                About {props.name} ?
+            </CoinDescriptionSubject>
+            <CoinDescriptionContent mode={modeState.mode}>
+                {props.description}
+            </CoinDescriptionContent>
+        </CoinDescriptionWrap>
+    )
+}
--- front/src/common/infomation/styled/index.ts
+++ front/src/common/infomation/styled/index.ts
@@ -1,3 +1,4 @@
 export * from "./CoinInfo.styled"
 export * from "./CoinInfoHeader.styled"
 export * from "./CoinInfoRow.styled"
+export * from "./CoinDescription.styled"
