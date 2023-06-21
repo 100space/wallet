4a2146067075b272dce71bed26788d957fa8e4a3
cloudcoke <lms4296@gmail.com>
2023-06-21 10:42:24 +0900

    Test : test back server 배포 테스트

--- back/src/health/health.controller.ts
+++ back/src/health/health.controller.ts
@@ -4,6 +4,6 @@ import { Controller, Get } from '@nestjs/common';
 export class HealthController {
   @Get()
   healthCheck(): string {
-    return 'Health Check Success!';
+    return 'Health Check Success';
   }
 }
