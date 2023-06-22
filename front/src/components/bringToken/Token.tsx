4d5e349aedc5cdee29971cf2d9790327f40415c0
chopssal <koalasunghee@gmail.com>
2023-06-23 00:06:48 +0900

    Feat: bringToken Component생성

---
+++ front/src/components/bringToken/Token.tsx
@@ -0,0 +1,12 @@
+import { TokenWrapper, TokenWrap } from "@components/bringToken/styled/index"
+
+export const Token = () => {
+
+    return(
+        <>
+            <TokenWrapper>
+                <TokenWrap></TokenWrap>
+            </TokenWrapper>
+        </>
+    )
+}
---
+++ front/src/components/bringToken/styled/Token.styled.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+
+export const TokenWrapper = styled.div`
+    width: 36rem;
+    height: 60rem;
+    background-color: #a7d500;
+`
+export const TokenWrap = styled.div`
+    
+`
