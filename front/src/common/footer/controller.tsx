01c371e6f07c5287faa397c7bbf4b031e4c50439
chopssal <koalasunghee@gmail.com>
2023-06-22 14:09:03 +0900

    Feat: Popup Component 완료

--- front/src/common/footer/controller.tsx
+++ front/src/common/footer/controller.tsx
@@ -1,8 +1,10 @@
 import { Footer } from "@components/Footer"
+import { Popup } from "@components/popup"
 
 export const Controller = () => {
     return (
         <>
+            <Popup/>
             <Footer />
         </>
     )
--- front/src/components/popup/stlyed/Popup.styled.tsx
+++
@@ -1,7 +0,0 @@
-import { styled } from "styled-components"
-
-export const PopupWrapper = styled.div`
-    width: 3.34rem;
-    height: 4.8rem;
-    background-color: #00465f;
-`
