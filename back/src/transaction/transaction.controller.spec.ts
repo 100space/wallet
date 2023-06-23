a9a5c00a99602b260d03e15ad4cc4d87b5c351d1
cloudcoke <lms4296@gmail.com>
2023-06-23 18:21:23 +0900

    Fix : 오류나는 Test Code 수정

--- back/src/transaction/transaction.controller.spec.ts
+++ back/src/transaction/transaction.controller.spec.ts
@@ -1,12 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TransactionController } from './transaction.controller';
 import { TransactionService } from './transaction.service';
+import { Web3Module } from '../web3/web3.module';
 
 describe('TransactionController', () => {
   let controller: TransactionController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [Web3Module],
       controllers: [TransactionController],
       providers: [TransactionService],
     }).compile();
--- back/src/trends/trends.service.spec.ts
+++ back/src/trends/trends.service.spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TrendsService } from './trends.service';
+import { HttpModule } from '@nestjs/axios';
 
 describe('TrendsService', () => {
   let service: TrendsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule],
       providers: [TrendsService],
     }).compile();
 
