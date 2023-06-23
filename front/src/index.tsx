7b3e85312e265e0e12785d8a98a596d1f91ace20
chopssal <koalasunghee@gmail.com>
2023-06-23 16:54:36 +0900

    Feat: QR Component & BottomSheet Component 작업 완료

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -9,7 +9,6 @@ import { RecoilRoot } from "recoil"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import { BrowserRouter } from "react-router-dom"
-import { BottomSheet } from "@common/bottomShett"
 
 const queryClient = new QueryClient()
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
@@ -21,8 +20,7 @@ root.render(
         <BrowserRouter>
             <QueryClientProvider client={queryClient}>
                 <ThemeProvider theme={theme}>
-                    {/* <App /> */}
-                    <BottomSheet/>
+                    <App />
                     {screenHeight > 600 && screenWidth > 800 && screenWidth > screenHeight ? (
                         <ReactQueryDevtools initialIsOpen={true} />
                     ) : (
