b13b91ee4e52ab9523a6e46c56e7e4c6a4f9de87
char1ey <ckstn410@gmail.com>
2023-06-26 17:38:00 +0900

    Feat: NFTInfomationImg 생성

--- front/src/components/Nft/NftInfoImg/NFTInfomationImg.tsx
+++ front/src/components/Nft/NftInfoImg/NFTInfomationImg.tsx
@@ -22,7 +22,7 @@ export const NFTINfomationImage = styled.img<ISizeProps>`
 
 export const NFTInfomationImg = () => {
     return (
-        <NFTInfomationImgWrap height={"80%"}>
+        <NFTInfomationImgWrap height={"60%"}>
             <NFTINfomationImage src="https://mblogthumb-phinf.pstatic.net/20150201_17/dahong02_1422800518504l5WCI_GIF/%BF%A1%BA%F1%C3%F2-%B8%C5%C1%FA.gif?type=w420" />
         </NFTInfomationImgWrap>
     )
