95d842cdb6e587af65125faff625384591dd7d2c
chopssal <koalasunghee@gmail.com>
2023-06-27 17:33:22 +0900

    Fix: Send Component 수정

--- front/src/components/PopupItem/sendComp/index.ts
+++ front/src/components/PopupItem/sendComp/index.ts
@@ -1,4 +1,1 @@
 export * from "./SendComp"
-export * from "./SubCon"
-export * from "./Subject"
-export * from "./Content"
---
+++ front/src/components/PopupItem/sendComp/styled/SendComp.styled.tsx
@@ -0,0 +1,13 @@
+import { styled } from "styled-components";
+
+export const SendCompWrapper = styled.div`
+    margin: 2rem;
+    display: flex;
+    flex-direction: column;
+    font-size: 2rem;
+    color: white;
+`
+
+export const SendCompWrap = styled.div`
+    padding: 0 2rem;
+`
