1c7fc7c15f89356ea2389c5ea539c1f921cff895
BAEK'space <112856532+100space@users.noreply.github.com>
2023-07-11 18:43:32 +0900

    Fix : GitActions Error

--- front/src/components/ThemeToggle/ThemeToggle.tsx
+++ front/src/components/ThemeToggle/ThemeToggle.tsx
@@ -1,56 +1,53 @@
 import { useRecoilState, useRecoilValue } from "recoil"
 import { ToggleWrapper } from "./styled"
-import { Icon } from '@iconify/react';
-import { useGetMode } from "@hooks/useMode";
-import { relative } from "path";
-import { useCallback, useState } from "react";
+import { Icon } from "@iconify/react"
+import { useGetMode } from "@hooks/useMode"
+import { relative } from "path"
+import { useCallback, useState } from "react"
 import styled, { ThemeProvider } from "styled-components"
-import { ModeState } from "@utils/localStorage";
-import { MouseEvent } from "react";
+import { ModeState } from "@utils/localStorage"
+import { MouseEvent } from "react"
 
 export const ThemeToggle = () => {
-const [mode, setMode] = useRecoilState(ModeState);
+    const [mode, setMode] = useRecoilState(ModeState)
 
-const toggleTheme = (e:MouseEvent) => {
-    if (mode.mode === "lightMode") {
-    setMode({...mode, mode:"darkMode"});
-    } else {
-    setMode({...mode, mode:"lightMode"});
+    const toggleTheme = (e: MouseEvent) => {
+        if (mode.mode === "lightMode") {
+            setMode({ ...mode, mode: "darkMode" })
+        } else {
+            setMode({ ...mode, mode: "lightMode" })
+        }
     }
-};
 
-
-return (
-    <>
-    <ToggleWrapper>
-        {mode.mode === "lightMode"? (
-        <Icon
-            onClick={toggleTheme}
-            icon="ph:moon"
-            style={{
-            color: "black",
-            fontSize: "3rem",
-            position: "relative",
-            top: "0.2rem",
-            margin: "0.3rem",
-            marginRight: "0.7rem"
-        }}
-        />
-        ) : (
-            <Icon
-            onClick={toggleTheme}
-            icon="ph:sun"
-            style={{
-                color: "#fff",
-                fontSize: "3rem",
-                position: "relative",
-                top: "0.2rem",
-                margin: "0.3rem",
-                marginRight: "0.7rem"
-            }}
-        />
-        )}
-    </ToggleWrapper>
-    </>
-);
-};
+    return (
+        <>
+            <ToggleWrapper>
+                {mode.mode === "lightMode" ? (
+                    <Icon
+                        onClick={toggleTheme}
+                        icon="ph:moon"
+                        style={{
+                            color: "black",
+                            fontSize: "3rem",
+                            position: "relative",
+                            top: "0.2rem",
+                            margin: "0.3rem",
+                            marginRight: "0.7rem",
+                        }}
+                    />
+                ) : (
+                    <Icon
+                        onClick={toggleTheme}
+                        icon="ph:sun"
+                        style={{
+                            color: "#fff",
+                            fontSize: "3rem",
+                            position: "relative",
+                            margin: "0.3rem",
+                        }}
+                    />
+                )}
+            </ToggleWrapper>
+        </>
+    )
+}
