93a192e5199a598c6677fd311e0b885fc575fd50
BAEK'space <112856532+100space@users.noreply.github.com>
2023-07-11 20:26:15 +0900

    Fix : BackCORS ERROR

--- back/src/main.ts
+++ back/src/main.ts
@@ -8,7 +8,13 @@ async function bootstrap() {
   app.use(cookieParser());
 
   app.enableCors({
-    origin: true,
+    origin: [
+      'http://localhost:3000',
+      'http://127.0.0.1:3000',
+      'https://nftin.com',
+      'https://www.nftin.com',
+      'https://front.nftin.com',
+    ],
     credentials: true,
   });
 
