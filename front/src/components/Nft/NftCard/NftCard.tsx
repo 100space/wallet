5c7aaf80699ab3983988c86ebd9726ecc2dc4db2
BAEK'space <112856532+100space@users.noreply.github.com>
2023-07-11 12:42:15 +0900

    Fix:color, nftinfo

--- front/src/components/Nft/NftCard/NftCard.tsx
+++ front/src/components/Nft/NftCard/NftCard.tsx
@@ -4,34 +4,55 @@ import { NftContents } from "./NftContents"
 import { NftCardWrap } from "./styled"
 import { MouseEvent, memo } from "react"
 import { useLocation } from "react-router"
+import { useRecoilValue } from "recoil"
+import { ModeState } from "@utils/localStorage"
 
-export const NftCard = memo((props: { nftInfo: INFTCardByMarket; className: string, onClick: (e: MouseEvent, ca: string, tokenId: number) => void }) => {
-    const { pathname } = useLocation()
+export const NftCard = memo(
+    (props: {
+        nftInfo: INFTCardByMarket
+        className: string
+        onClick: (e: MouseEvent, ca: string, tokenId: number) => void
+    }) => {
+        const { pathname } = useLocation()
+        const { mode } = useRecoilValue(ModeState)
+        const getRandomColor = () => {
+            const darkLetters = "0123456789a"
+            const lightLetters = "56789ABCDEF"
+            let letters = mode === "darkMode" ? darkLetters : lightLetters
+            let color = "#"
 
-    const getRandomColor = () => {
-        const letters = "0123456789ABCDEF"
-        let color = "#"
-        for (var i = 0; i < 6; i++) {
-            color += letters[Math.floor(Math.random() * 16)]
+            for (var i = 0; i < 6; i++) {
+                color += letters[Math.floor(Math.random() * 10)]
+            }
+            return color
         }
-        return color
-    }
 
-    return pathname.indexOf("/market/new") >= 0 || pathname.indexOf("/market") <= 0 ? (
-        <NftCardWrap color={getRandomColor()} width={"100%"} height={"26rem"} onClick={(e) => {
-            if (!props.nftInfo.nftAddress || !props.nftInfo.tokenId) return
-            return props.onClick(e, props.nftInfo.nftAddress, props.nftInfo.tokenId)
-        }}>
-            <NftCardImg width={"90%"} height={"60%"} image={props.nftInfo.image} className={props.className} />
-            <NftContents name={props.nftInfo.name} owner={props.nftInfo.owner} prices={props.nftInfo.prices} />
-        </NftCardWrap>
-    ) : (
-        <NftCardWrap color={getRandomColor()} width={"50rem"} height={"25rem"} onClick={(e) => {
-            if (!props.nftInfo.nftAddress || !props.nftInfo.tokenId) return
-            return props.onClick(e, props.nftInfo.nftAddress, props.nftInfo.tokenId)
-        }}>
-            <NftCardImg width={"90%"} height={"60%"} image={props.nftInfo.image} className={props.className} />
-            <NftContents name={props.nftInfo.name} owner={props.nftInfo.owner} prices={props.nftInfo.prices} />
-        </NftCardWrap>
-    )
-})
+        return pathname.indexOf("/market/new") >= 0 || pathname.indexOf("/market") <= 0 ? (
+            <NftCardWrap
+                color={getRandomColor()}
+                width={"100%"}
+                height={"26rem"}
+                onClick={(e) => {
+                    if (!props.nftInfo.nftAddress || !props.nftInfo.tokenId) return
+                    return props.onClick(e, props.nftInfo.nftAddress, props.nftInfo.tokenId)
+                }}
+            >
+                <NftCardImg width={"90%"} height={"60%"} image={props.nftInfo.image} className={props.className} />
+                <NftContents name={props.nftInfo.name} owner={props.nftInfo.owner} prices={props.nftInfo.prices} />
+            </NftCardWrap>
+        ) : (
+            <NftCardWrap
+                color={getRandomColor()}
+                width={"50rem"}
+                height={"25rem"}
+                onClick={(e) => {
+                    if (!props.nftInfo.nftAddress || !props.nftInfo.tokenId) return
+                    return props.onClick(e, props.nftInfo.nftAddress, props.nftInfo.tokenId)
+                }}
+            >
+                <NftCardImg width={"90%"} height={"60%"} image={props.nftInfo.image} className={props.className} />
+                <NftContents name={props.nftInfo.name} owner={props.nftInfo.owner} prices={props.nftInfo.prices} />
+            </NftCardWrap>
+        )
+    }
+)
