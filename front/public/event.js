21fbaf0660ec21f3fa17d5cea9fc616fcd47f8bf
BAEK'space <112856532+100space@users.noreply.github.com>
2023-07-13 19:14:56 +0900

    Feat : Extention

--- front/public/event.js
+++ front/public/event.js
@@ -9,6 +9,7 @@ window.addEventListener("message", (event) => {
     if (event.source !== window) return
     const { type, id, method, params, from } = event.data
     if (type === "fromClient" && from === "windowproperty") {
+        chrome.runtime.sendMessage({ type: "openPopup" })
         chrome.runtime.sendMessage({ from: "event", type: "fromEvent", method, params }, (resp) => {
             console.log(resp, "resp in envet.js", 11111111)
             // window.postMessage({ type: "fromEvent", id, response: resp.response, from: "event" }, "*")
