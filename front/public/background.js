afb5c46a97e7c99cfaaaf0ae9de5e5befc859a76
BAEK'space <112856532+100space@users.noreply.github.com>
2023-07-14 15:48:07 +0900

    Fix : 확장프로그램

--- front/public/background.js
+++ front/public/background.js
@@ -1,12 +1,15 @@
 /* eslint-disable no-undef */
 //background.js
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    const { from, type, method, params } = request
-    if (from && from === "event") {
-        chrome.runtime.sendMessage({ from: "background", type: "fromBackground", method, params }, (resp) => {
-            resp && sendResponse({ type: "res_external", response: resp.response })
-        })
-    }
-    return true // Will respond asynchronously.
-})
+const userAgent = navigator.userAgent.toLowerCase()
+const isChrome = userAgent.includes("chrome")
+if (isChrome) {
+    chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+        const { from, type, method, params } = request
+        if (from && from === "event") {
+            chrome.runtime.sendMessage({ from: "background", type: "fromBackground", method, params }, (resp) => {
+                resp && sendResponse({ type: "res_external", response: resp.response })
+            })
+        }
+        return true // Will respond asynchronously.
+    })
+}
--- front/public/external.js
+++ front/public/external.js
@@ -1,34 +1,38 @@
 /* eslint-disable no-undef */
 // external.js
-let checkInterval = setInterval(() => {
-    if (window.abc) {
-        console.log("Ethereum object is available!")
-        clearInterval(checkInterval) // 지갑이 준비되었으므로 체크를 멈춥니다.
-        chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-            const { from, type, method, params } = request
-            if (from && from === "background") {
-                if ((method === "eth_requestAccounts") | (method === "eth_accounts")) {
-                    window.abc.wallet.getAddress().then((address) => {
-                        sendResponse({ type: "res_external", response: [address] })
-                    })
-                } else if (method.includes("eth_")) {
-                    let requestMethod = method
-                    let params = request.params
-                    if (method === "eth_sendTransaction") {
-                        window.abc.wallet.sendTransaction(params[0]).then((response) => {
-                            const { hash } = response
-                            sendResponse({ type: "res_external", response: hash })
+const userAgent = navigator.userAgent.toLowerCase()
+const isChrome = userAgent.includes("chrome")
+if (isChrome) {
+    let checkInterval = setInterval(() => {
+        if (window.abc) {
+            console.log("Ethereum object is available!")
+            clearInterval(checkInterval) // 지갑이 준비되었으므로 체크를 멈춥니다.
+            chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+                const { from, type, method, params } = request
+                if (from && from === "background") {
+                    if ((method === "eth_requestAccounts") | (method === "eth_accounts")) {
+                        window.abc.wallet.getAddress().then((address) => {
+                            sendResponse({ type: "res_external", response: [address] })
                         })
-                    } else {
-                        window.abc.provider.send(requestMethod, params).then((response) => {
-                            sendResponse({ type: "res_external", response })
-                        })
+                    } else if (method.includes("eth_")) {
+                        let requestMethod = method
+                        let params = request.params
+                        if (method === "eth_sendTransaction") {
+                            window.abc.wallet.sendTransaction(params[0]).then((response) => {
+                                const { hash } = response
+                                sendResponse({ type: "res_external", response: hash })
+                            })
+                        } else {
+                            window.abc.provider.send(requestMethod, params).then((response) => {
+                                sendResponse({ type: "res_external", response })
+                            })
+                        }
                     }
                 }
-            }
-            return true
-        })
-    } else {
-        console.log("Ethereum object is not available yet.")
-    }
-}, 1000) // 매 초마다 체크합니다.
+                return true
+            })
+        } else {
+            console.log("Ethereum object is not available yet.")
+        }
+    }, 1000) // 매 초마다 체크합니다.
+}
